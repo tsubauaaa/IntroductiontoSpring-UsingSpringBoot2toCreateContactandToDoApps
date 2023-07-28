2391e7017077a17584129041c2fa0448f356eee5
tsubauaaa <tsubasa1173@gmail.com>
2023-07-28 17:19:40 +0900

    completed

---
+++ WebToDoStarter/src/main/java/com/example/demo/WebToDoStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToDoStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebToDoStarterApplication.class, args);
+	}
+
+}
